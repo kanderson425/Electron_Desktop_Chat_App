aff7b5cf1961c720269b7c818c781299aed21eaf
kanderson425 <kyleanderson.dev@gmail.com>
2019-05-14 20:55:36 -0500

    First commit

---
+++ server.js
@@ -0,0 +1,40 @@
+// ./server.js
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const Chatkit = require('pusher-chatkit-server')
+
+const chatkit = new Chatkit.default({
+  instanceLocator: 'v1:us1:a58539cb-5e05-48db-b90c-552555b9d79b',
+  key:
+    'b81fbcb4-7cf8-4e0e-9762-ea41bb3b1b27:sulPnmIacKY66yVrhdOlkzId4M92BPsFame6qY/B35w='
+})
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.post('/users', (req, res) => {
+  const { username } = req.body
+  const user = { name: username, id: username }
+  chatkit
+    .createUser(user)
+    .then(() => {
+      console.log('Created user ', user.name)
+      res.status(201).json(user)
+    })
+    .catch(error => {
+      if (error.error === 'services/chatkit/user_already_exists') {
+        console.log('User already exists ', user.name)
+        res.status(201).json(user)
+      } else {
+        console.error(error)
+        res.status(error.status).json(error)
+      }
+    })
+})
+
+app.listen(3001)
+console.log('Running on port 3001')
