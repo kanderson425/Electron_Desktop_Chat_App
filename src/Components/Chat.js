3b2a69e6a1da16dcf6d7b7be3120c96c0f1b3f42
kanderson425 <kyleanderson.dev@gmail.com>
2019-05-14 21:22:45 -0500

    Adjusts the Chat component

--- src/Components/Chat.js
+++ src/Components/Chat.js
@@ -1,12 +1,36 @@
 // ./src/Chat.js
 
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { ChatManager, TokenProvider } from '@pusher/chatkit-client';
 
 class Chat extends Component {
+    state = {
+        currentUser: null
+    }
+
+    componneDidMount() {
+        const chatKit = new ChatManager({
+            instanceLocator: 'v1:us1:a58539cb-5e05-48db-b90c-552555b9d79b',
+            userId: this.props.currentId,
+            tokenProvider: new TokenProvider({
+                url:
+                'https://us1.pusherplatform.io/services/chatkit_token_provider/v1/a58539cb-5e05-48db-b90c-552555b9d79b/token'
+            })
+        })
+
+        chatKit
+        .connect()
+        .then(currentUser => {
+            this.setState({ currentUser })
+            console.log('Bleep bloop you are connected to Chatkit')
+        })
+        .catch(error => console.error('error', error))
+    }
+
   render() {
     return (
       <div>
-        <h1>Chat</h1>
+        <h1>Chat Screen</h1>
       </div>
     )
   }
